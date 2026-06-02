b62e23f8646b0853b20181717707772c2c893826
sofiaanglarill <sofia.anglarill@gmail.com>
2026-06-03 00:47:52 +0200

    se fijo una altura en el slider para que este no se desconfigure, hay algunas imagenes de diferentes tamaño, igual se cambio el estilo de transcición

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 document.querySelectorAll(".slider").forEach((slider) => {
   const imgs = slider.querySelectorAll("img");
   let i = 0;
+
+  imgs.forEach((img, index) => {
+    img.style.position = "absolute";
+    img.style.top = "0";
+    img.style.left = index === 0 ? "0%" : "100%";
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.objectFit = "contain";
+    img.style.transition = "left 0.6s ease";
+    img.style.display = "block";
+  });
+
   setInterval(() => {
-    imgs[i].classList.remove("activa");
+    const current = imgs[i];
     i = (i + 1) % imgs.length;
-    imgs[i].classList.add("activa");
+    const next = imgs[i];
+
+    next.style.transition = "none";
+    next.style.left = "100%";
+
+    setTimeout(() => {
+      next.style.transition = "left 0.6s ease";
+      current.style.left = "-100%";
+      next.style.left = "0%";
+    }, 20);
   }, 2500);
 });
